bf44d369a7994b9294a6f6f7a6abc5e521a658b7
jennydunix <jennifer.o.odunze@gmail.com>
2023-06-07 22:51:26 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import "dotenv/config";
+import connectDB from "./utils/db.js";
+import Links from "./models/Link.js"
+const app = express();
+
+// connect to database
+connectDB();
+
+const port = process.env.PORT;
+
+app.get("/:id", async (req, res) => {
+	try {
+		const link = await Links.findOne({ shortId: req.params.id });
+
+		if (!link) {
+			res.json({ msg: "Invalid url" });
+			return;
+		}
+
+		await Links.updateOne({ _id: link._id }, { $inc: { no_of_visits: 1 } });
+
+		res.status(200).redirect(link.longUrl);
+	} catch (error) {
+		res.status(400).json({ errorMessage: error.message });
+	}
+});
+
+app.listen(port, () => {
+	console.log("Node is working ");
+});
